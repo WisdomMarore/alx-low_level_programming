f684a324dd09b819373d582fa93d588e358247ce
Wisdom Marore <marorew@gmail.com>
2023-08-14 08:55:55 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ *main  - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
-int main(void)
+int main (void)
 {
 	char c;
 
 	for (c = 'a';c <= 'z'; c++)
 	{
-		for (c = 'A';c <= 'Z'; c++)
-		{
-			putchar(c);
-			return (0);
-		}
-		        putchar(c);
-			return (0);
-
+		putchar(c);
 	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+        putchar('\n');
+	return (0);
 }
