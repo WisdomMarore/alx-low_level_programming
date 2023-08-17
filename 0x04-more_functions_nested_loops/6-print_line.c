7d9a6e547110bce2be9036a2fcc76a3184172256
Wisdom Marore <marorew@gmail.com>
2023-08-17 10:49:37 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0;i < n; i++)
+	if (n <= 0)
 	{
-		if (n > 0)
-		{
-			_putchar('_');
-			_putchar('\n');
-	        }
-		else
-		{
 		_putchar('\n');
-		}
+		return;
 	}
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
