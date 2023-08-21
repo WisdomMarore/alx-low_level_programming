9c45345f266a355d51ede48baced72c7768a3f94
Wisdom Marore <marorew@gmail.com>
2023-08-21 08:34:24 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int an updates the value to 98
  *@:*n pointer to the integer
- *@:n variable n
+ *@:n variable n to be inputed
  *
  * Return: Always 0 (Success)
  */
