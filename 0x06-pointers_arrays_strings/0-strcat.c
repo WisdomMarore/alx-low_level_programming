f5d316df3443ae4d9bc18f84551d8ef3e02b570f
Wisdom Marore <marorew@gmail.com>
2023-08-23 00:41:54 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat - joins two strings together
+ * @dest:first string
+ * @src:second string
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+	*p = '\0';
+	return (dest);
+}
