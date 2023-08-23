d639148670635ffdd4c91ad37ccbe4e822c4d679
Wisdom Marore <marorew@gmail.com>
2023-08-23 00:54:34 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - joins two strings together
+ *@dest:first string
+ *@src:second string
+ *@n:number of bytes
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (n-- > 0 && *src != '\0')
+	{
+		*p++ = *src;
+	}
+	*p = '\0';
+	return (dest);
+}
