3be488bad6789c05905874e6d50ada7ce1550005
Wisdom Marore <marorew@gmail.com>
2023-08-23 02:06:57 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes string lowercases to uppercases
+ * @* - the string
+ *
+ * Return: Always 0 (Success)
+ */
+char *string_toupper(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		if (*p >= 'a' && *p <= 'z')
+		{
+			*p = *p - 'a' + 'A';
+		}
+		p++;
+	}
+	return (s);
+}
