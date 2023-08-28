d9af950b5116451d188a33fb52b44853e135cc70
Wisdom Marore <marorew@gmail.com>
2023-08-28 08:25:03 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies the memory area
+ * @dest: the destination
+ * @src: the source
+ * @n: the number of bytes
+ *
+ * Return Always 0 (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+	
+	while (n--)
+	{
+		*p++ = *src++;
+	}
+	return (dest);
+}
