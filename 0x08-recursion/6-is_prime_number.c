99b1450bda074a22debde5985b9185ad53c271a2
Wisdom Marore <marorew@gmail.com>
2023-08-30 10:06:24 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,16 +8,10 @@
  */
 int is_prime_number(int n)
 {
-	if (n < 0)
-	{
+	if (n <= 1)
 		return (0);
-	}
-	if (n == 1)
-	{
+	if (n % 1 == 0 && n % n == 0)
 		return (1);
-	}
-	if (n % n == 0 && n % 1 == 0)
-	{
-		return (1);
-	}
+	return (50);
+	
 }
