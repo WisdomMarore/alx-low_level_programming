61b2b4ded0d6c0d776a8736caa9cae3d86404e60
Wisdom Marore <marorew@gmail.com>
2023-08-30 01:50:44 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: the string
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
